d2d0ad7974fdb0caa08c9066d07f0e076dd64563
iamShah <formal>
2020-11-12 19:43:00 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
-end
+
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array = array.join("") 
+      array.insert(-7," and ")
+    elsif array.length == 3
+      array = array.join(", ") 
+      array.insert(-10,"and ")
+    elsif array.length == 5
+      array = array.join(", ") 
+      array.insert(-14, "and ")
+    end
+end    
